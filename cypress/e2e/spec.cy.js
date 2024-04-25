4aed65b7a8944abe2c61b1af41b81236ff8eb405
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-04-25 14:27:06 +0300

    11.9 Simple end-to-end tests CI/CD

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  /* it('front page can be opened', function() {
     cy.visit('http://localhost:8080/')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-  /* it('pokemon page can be navigated to', function() {
+  }) */
+  it('pokemon page can be navigated to', function() {
     cy.visit('http://localhost:5000/pokemon/ivysaur')
     cy.contains('overgrow')
     cy.contains('chlorophyll')
-  }) */
+  })
 })
